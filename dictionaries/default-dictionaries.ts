215e3ac2242ba44950479d15af883466e2cffe9b
LucasDiello <lucasoliveiradiello@hotmail.com>
2024-09-03 17:27:15 -0300

    remove posts

--- dictionaries/default-dictionaries.ts
+++
@@ -1,7 +0,0 @@
-import { en } from "./default-language-collections/default-en";
-import { pt } from "./default-language-collections/default-pt";
-
-export const defaultDictionary = {
-  'en': en,
-  'pt': pt,
-};
