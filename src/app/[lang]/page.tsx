026c50f44190af01f63f6620665815d6f6dde32a
LucasDiello <lucasoliveiradiello@hotmail.com>
2024-10-16 14:04:47 -0300

    increment title & icon metadata

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,5 @@
-import AsideLeft from '@/components/Aside-left/AsideLeft';
 import Projects from '@/components/projecst/Projects';
 import { Locale } from '@/config/i18n.config';
-import { getDictionaryServerOnly } from '@/dictionaries/default-dictionary-server-only';
 import React from 'react';
 
 export default function Home({ params }: { params: { lang: Locale } }) {
